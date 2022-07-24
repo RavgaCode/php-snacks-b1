c5b900b34702b6ce7c7d17d21937fc1f4d212582
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-24 22:27:01 +0200

    tag fix

--- snack-4/index.php
+++ snack-4/index.php
@@ -10,7 +10,7 @@
     <?php 
     $bigParagraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, magni enim. Harum totam repellat ipsa qui quia autem, architecto sequi voluptas cupiditate laboriosam adipisci. Omnis consequuntur ipsum illum magnam molestias. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ex tenetur architecto voluptate quia praesentium sit? Sapiente, nemo praesentium vitae quaerat, voluptatum asperiores repellendus saepe aperiam excepturi necessitatibus porro aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum magni quae similique necessitatibus beatae aperiam possimus inventore nostrum repellendus dolore nobis, nemo ipsum ratione harum ea aspernatur consequatur adipisci numquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam aperiam tempore dolor sint exercitationem accusantium quidem totam, nulla praesentium illum natus eligendi voluptatum perferendis ipsam quas, repellat repellendus. Iste, officia.";
     
-    $replacedString = str_replace(".",".<br>",$bigParagraph);
+    $replacedString = str_replace(".",".</p><p>",$bigParagraph);
     
     ?>
     <p>
